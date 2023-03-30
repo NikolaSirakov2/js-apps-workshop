1eb2b0c2dc434dab23bb61740731366da30e7861
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-03-31 00:14:42 +0300

    update

--- MyCookbook/app.js
+++ MyCookbook/app.js
@@ -1,25 +1,80 @@
-let test = fetch(`http://localhost:3030/jsonstore/cookbook/recipes`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let article = document.getElementById("recepiesList");
+class ViewController {
+  constructor() {
+    window.addEventListener("load", this.handleHashChange);
+    window.addEventListener("hashchange", this.handleHashChange);
+  }
 
-    console.log(data["03"]);
+  handleHashChange = (e) => {
+    const hash = location.hash.slice(1);
 
-    for (let i = 1; i < 4; i++) {
-      let card = document.createElement("div");
-      let image = data[`0${i}`].img;
-      let name = data[`0${i}`].name;
+    // if (!PAGE_IDS.includes(hash)) {
+    //   location.hash = "error";
+    //   return;
+    // }
 
-      card.innerHTML = `<div class="card" style="width: 28rem;">
+    PAGE_IDS.forEach((pageId) => {
+      let element = document.getElementById(pageId);
+      if (hash === pageId) {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    });
+
+    switch (hash) {
+      
+      case "recepies":
+        this.renderRecepiesListPage();
+        break;
+      case "details":
+        this.renderDetailsPage();
+        break;
+    }
+  };
+
+  renderRecepiesListPage = () => {
+    let test = fetch(`http://localhost:3030/jsonstore/cookbook/recipes`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let article = document.getElementById("recepiesList");
+
+        console.log(data["03"]);
+
+        for (let i = 1; i < 4; i++) {
+          let card = document.createElement("div");
+          let image = data[`0${i}`].img;
+          let name = data[`0${i}`].name;
+
+          card.innerHTML = `<div class="card" style="width: 28rem;">
             <img src="${image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${name}</h5>
-              <a href="#" class="btn btn-primary">Details</a>
+              
             </div>
           </div>`;
 
-      article.append(card);
-    }
-  });
+          let detailsBtn = document.createElement("a");
+          detailsBtn.classList.add("btn", "btn-primary");
+          detailsBtn.innerText = "Details";
+
+          detailsBtn.onclick = (e) => {
+            e.preventDefault();
+         console.log(5);
+         location.hash = "details";
+            this.renderDetailsPage(data[`0${i}`]._id);
+            
+          };
+
+          card.appendChild(detailsBtn)
+
+          article.append(card);
+        }
+      });
+  };
+
+  renderDetailsPage = (id) => {};
+}
+
+let viewController = new ViewController();
